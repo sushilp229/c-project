751f01380503079f652af7460f408624a447aa98
abc <sushil@gmail.com>
2023-07-02 18:57:15 +0000

    added all my c file

---
+++ pal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str[10] = "naman";
+    int i, len, flag = 0;
+    
+    len = strlen(str);
+
+    for (i = 0; i < len; i++) 
+    {
+        // Checking if string is palindrome or not
+        if (str[i] != str[len - i - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("%s is not palindrome", str);
+    else
+        printf("%s is palindrome", str);
+        
+    return 0;
+}
